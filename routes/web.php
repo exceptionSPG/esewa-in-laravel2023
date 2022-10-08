42cb8c57413fa3cebe05f6b4e93ff45cc2c44036
Shiva Prasad Gyawali <gyawalishiva14@gmail.com>
2022-10-08 17:20:41 +0545

    esewa implementation success

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EsewaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/esewa', [EsewaController::class, 'esewaPay'])->name('esewa');
+Route::get('/success', [EsewaController::class, 'esewaPaySuccess']);
+Route::get('/failure', [EsewaController::class, 'esewaPayFailed']);
